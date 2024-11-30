61adf188c2852ed1b78631e0c22a03fee7847e36
Yves Antônio <yves@gec.inatel.br>
2024-11-29 21:57:48 -0300

    fix: port errror

--- src/http/server.ts
+++ src/http/server.ts
@@ -35,8 +35,8 @@ app.register(updateProjectionRoute)
 app.register(deleteProjectionRoute)
 
 app.listen({
+  host: '0.0.0.0',
   port: process.env.PORT ? Number(process.env.PORT) : 3333
 }).then(() => {
   console.log('HTTP server running')
-  console.log(`Server is running on port ${process.env.PORT ? Number(process.env.PORT) : 3333}`)
 })
